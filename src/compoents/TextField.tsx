e5f914d36afa1b9ca56618994b3475796c9acdbb
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-11-24 13:45:43 +0530

    Passing object.

--- src/compoents/TextField.tsx
+++ src/compoents/TextField.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
+interface Person {
+    firstName: string;
+    lastName: string;
+}
 interface Props {
     text: string;
     ok?: boolean;       //? => means, not necessary to pass.
     num?: number;
     funStr ?: (name: string) => string;
+    obj: Person;
 }
 
 const TextField: React.FC<Props> = (Props) => {    
